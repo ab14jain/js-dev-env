08542e5b26ccc1faeffa3f0fbe01d8905ef68725
Abhishek <ab14jain@gmail.com>
2018-03-17 16:56:48 +0530

    mock data api call added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import './index.css';
+import{getUsers} from './api/userApi';
 import numeral from 'numeral';
+
+getUsers().then(result => {
+    let userBody = "";
+    result.forEach(user => {
+        userBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+    });
+    global.document.getElementById('users').innerHTML = userBody; 
+});
+
 
 const courseValue = numeral(1000).format('$0,0.00');
 //debugger;
